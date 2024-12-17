5c6341f07d93153d0ff2e93aa09851abcb9c7061
s.diasamidze <saba2006.diasamidze@gmail.com>
2024-12-17 20:10:32 +0400

    implemented fps solution

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #makes a clock object for later limiting fps
+    clock = pygame.time.Clock()
+    dt = 0
+
     #game loop
     while True:
         #background color
         screen.fill((0, 0, 0))
+
+        #calculates delta
+        dt = clock.tick() / 1000
 
         #makes the window close button work
         for event in pygame.event.get():
